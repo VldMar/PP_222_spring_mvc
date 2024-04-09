93be0a8fb8317605fe82a74962fe781a781e2f07
Vladimir <marychev21@gmail.com>
2024-04-09 22:24:54 +0300

    Bug fixed

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-
-import java.util.List;
 
 @Controller
 public class CarsController {
@@ -18,8 +15,7 @@ public class CarsController {
 
     @GetMapping("/cars")
     public String showAllCars(@RequestParam(required = false, defaultValue = "5") Integer count, ModelMap model) {
-        List<Car> cars = carService.getCars(count);
-        model.addAttribute("cars", cars);
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
